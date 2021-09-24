f723917800b55f249c30525b4fbb320027bed6f6
LBJZN <2625680460@qq.com>
2021-09-24 13:52:23 +0800

    测试3

--- src/views/system3/index.vue
+++ src/views/system3/index.vue
@@ -40,6 +40,15 @@
           </template>
         </el-table-column>
       </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
     </el-card>
 
     <!--添加角色的对话框-->
